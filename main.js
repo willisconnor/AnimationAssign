f78756c3c72d98ac813da3f6d288794d6bef73fd
Connor Willis <connorwillis05@gmail.com>
2026-01-22 16:12:05 -0800

    oops

--- main.js
+++ main.js
@@ -2,10 +2,14 @@ const gameEngine = new GameEngine();
 
 const ASSET_MANAGER = new AssetManager();
 
+ASSET_MANAGER.queueDownload("./fixed_spritesheet.png");
+
 ASSET_MANAGER.downloadAll(() => {
 	const canvas = document.getElementById("gameWorld");
 	const ctx = canvas.getContext("2d");
+	
 	gameEngine.addEntity(new Ichigo());
+	
 	gameEngine.init(ctx);
 
 	gameEngine.start();

f2830eb4e32ca2f3027db07ac74d8c33f840e655
Connor Willis <connorwillis05@gmail.com>
2026-01-22 17:12:58 -0800

    i know the animation isnt gonna be in the right palce but here we go

--- ichigo.js
+++ ichigo.js
@@ -1,6 +1,7 @@
 class Ichigo {
     constructor() {
-
+        this.game = game;
+        this.animator = new Animator(ASSET_MANAGER.getAsset("/AnimationAssign/fixed_spritesheet.png"), 0, 0, 48, 72, 3, 0.2);
     };
 
     update(){
@@ -9,8 +10,7 @@ class Ichigo {
     };
 
     draw(ctx){
-        ctx.drawImage(ASSET_MANAGER.getAsset("/AnimationAssign/fixed_spritesheet.png"),0,0);
-
+        this.animator.drawFrame(this.game.clockTick, ctx, 100, 100);
     };
 
 }
--- main.js
+++ main.js
@@ -8,7 +8,7 @@ ASSET_MANAGER.downloadAll(() => {
 	const canvas = document.getElementById("gameWorld");
 	const ctx = canvas.getContext("2d");
 	
-	gameEngine.addEntity(new Ichigo());
+	gameEngine.addEntity(new Ichigo(gameEngine));
 	
 	gameEngine.init(ctx);
 

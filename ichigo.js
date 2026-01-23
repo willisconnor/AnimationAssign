0797d7bcc73561c3c21dc33e1ad02866a6913308
Connor Willis <connorwillis05@gmail.com>
2026-01-22 17:35:25 -0800

    bounds

--- ichigo.js
+++ ichigo.js
@@ -1,7 +1,7 @@
 class Ichigo {
     constructor(game) {
         this.game = game;
-        this.animator = new Animator(ASSET_MANAGER.getAsset("/AnimationAssign/fixed_spritesheet.png"), 0, 0, 48, 72, 5, 0.2);
+        this.animator = new Animator(ASSET_MANAGER.getAsset("/AnimationAssign/fixed_spritesheet.png"), 10, 10, 48, 72, 5, 0.2);
     };
 
     update(){

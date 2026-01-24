af1087a3bfa5dd341c63500cccdbb172746e1a70
Connor Willis <connorwillis05@gmail.com>
2026-01-24 02:17:45 -0800

    ok

--- ichigo.js
+++ ichigo.js
@@ -3,13 +3,14 @@ class Ichigo {
         this.game = game;
         this.animator = new Animator(ASSET_MANAGER.getAsset("/AnimationAssign/fixed_spritesheet.png"), 2, 196, 64, 48, 3, 0.2);
         
-        this.speed = 25;
+        this.speed = 200;
         this.x = 0;
         this.y = 0;
     };
 
     update(){
         this.x += this.speed * this.game.clockTick;
+        if(this.x > 1024) this.x = 0;
 
 
     };
